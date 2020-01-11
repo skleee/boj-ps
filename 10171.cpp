818176add4da9e9f61c76bfb48c702b51f900e2f
skleee <leesk1027@gmail.com>
2020-01-11 15:25:31 +0900

    0111 고양이 - 출력

---
+++ 10171.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);cout.tie(NULL);
+	cout << "\\    /\\" << "\n";
+	cout << " )  ( ')" << "\n";
+	cout << "(  /  )" << "\n";
+	cout << " \\(__)|" << "\n";
+	return 0;
+}
