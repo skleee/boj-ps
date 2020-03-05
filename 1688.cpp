66d400e9ef322e30e396e05a809d92fa5a927b69
skleee <leesk1027@gmail.com>
2020-03-05 15:06:51 +0900

    0305 지민이의 테러 - Geometry(Inside-polygon test - RayCasting Algorithm) 주석 추가

--- 1688.cpp
+++ 1688.cpp
@@ -5,6 +5,7 @@
 /*
 	1688. 지민이의 테러
 	Geometry
+	Inside-Polygon test (Ray casting algorithm)
 */
 using namespace std;
 
@@ -14,6 +15,7 @@ struct Point {
 vector<Point> v;
 int N;
 
+// struct Point의 x, y 좌표를 비교하는 함수. (기준 1: x 좌표, 기준 2: y 좌표)
 bool comparePoint(Point a, Point b) {
 	return a.x < b.x || (!(a.x > b.x) && a.y < b.y);
 }
@@ -40,20 +42,30 @@ bool isIntersect(Point l11, Point l12, Point l21, Point l22) {
 	else return true;
 }
 
+// 점이 내부에 있는지 확인하는 함수
 bool isInside(long long xx, long long yy) {
 	int cnt = 0;
-	Point tmp = { xx, yy };
+	Point tmp = { xx, yy }; // 검사하려는 점
 
 	for (int i = 0; i < N; i++) {
 		Point v1 = v[i];
 		Point v2 = v[(i + 1) % N];
-		Point end = { MAXVAL,  yy + 1 }; // 다른 정점과 겹치지 않도록
-	
+		Point end = { MAXVAL,  yy + 1 };
 		if (isIntersect(v1, v2, tmp, end)) 	cnt++;
+		/*
+			다각형의 꼭짓점은 항상 시계방향 또는 시계 반대방향 순으로 주어지기 때문에 차례대로 검사한다.
+			v[0]&v[1], v[1]&v[2], v[2]&v[3] ... v[N-1]&v[0] 형태로 검사하기 위해 mod 연산으로 v2의 인덱스를 구한다.
+			다각형의 내부, 외부를 판별하기 위해 검사하는 점과 다각형 바깥의 점을 하나 찍어 선을 긋는다.
+			이때 '다각형 바깥에 있는 점'의 x좌표는 좌표 범위보다 1 큰 값으로, 입력값으로 존재할 수 없는 값으로 한다.
+			y좌표는 검사할 점의 y좌표+1을 한 값으로 해서, 절대 꼭짓점과 겹치거나 직선이 변과 겹치게 되는 경우가 없게 한다.
+			그은 선이 다각형의 변과 만나는 횟수가 홀수이면 내부, 짝수이면 외부에 있는 것이다.
+			선분 교차 여부는 CCW로 판별한다. CCW 함수 내에서 일직선인 경우 false가 반환되도록 하여, 검사할 점이 다각형의 변 위에 있는 경우가 고려된다.
+		*/
 	}
 	return cnt % 2;
 }
 
+// 같은 좌표의 꼭짓점이 있는지 확인하는 함수
 bool isPoint(long long xx, long long yy) {
 	for (int i = 0; i < N; i++) {
 		if (v[i].x == xx && v[i].y == yy) return true;
@@ -79,7 +91,9 @@ int main() {
 			continue;
 		}
 
+		// 먼저 다각형 꼭짓점과 겹치는지 확인. 겹친다면 경계 위에 있는 것이므로 1 반환
 		if (isPoint(xx, yy)) cout << "1\n";
+		// 내부에 있는지 확인
 		else {	
 			cout << isInside(xx, yy) << "\n";
 		}
